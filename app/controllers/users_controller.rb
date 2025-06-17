a29836a003f52e9fb994030d1b10ade9d1c34091
Qwiet AI <noreply@shiftleft.io>
2025-06-17 15:16:51 +0530

    Fixing app/controllers/users_controller.rb for finding 45

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,25 +23,28 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
-  def update
-    message = false
+def update
+  message = false
 
-    user = User.where("id = '#{params[:user][:id]}'")[0]
+  # Fix SQL injection by using parameterized query instead of string interpolation
+  user = User.where(id: params[:user][:id]).first
 
-    if user
-      user.update(user_params_without_password)
-      if params[:user][:password].present? && (params[:user][:password] == params[:user][:password_confirmation])
-        user.password = params[:user][:password]
-      end
-      message = true if user.save!
-      respond_to do |format|
-        format.html { redirect_to user_account_settings_path(user_id: current_user.id) }
-        format.json { render json: {msg: message ? "success" : "false "} }
-      end
-    else
-      flash[:error] = "Could not update user!"
-      redirect_to user_account_settings_path(user_id: current_user.id)
+  if user
+    user.update(user_params_without_password)
+    if params[:user][:password].present? && (params[:user][:password] == params[:user][:password_confirmation])
+      user.password = params[:user][:password]
     end
+    message = true if user.save!
+    respond_to do |format|
+      format.html { redirect_to user_account_settings_path(user_id: current_user.id) }
+      format.json { render json: {msg: message ? "success" : "false "} }
+    end
+  else
+    flash[:error] = "Could not update user!"
+    redirect_to user_account_settings_path(user_id: current_user.id)
+  end
+end
+
   end
 
   private
